27a743686ddea8a702bf0da6c0a905fe0908c800
Adi Ciausu <adrian.ciausu@cegeka.com>
2019-06-20 13:28:15 +0300

    fixes

--- src/main/java/com/cegeka/tag/tagapi/dto/ImageDTO.java
+++ src/main/java/com/cegeka/tag/tagapi/dto/ImageDTO.java
@@ -1,18 +1,18 @@
 package com.cegeka.tag.tagapi.dto;
 
 import com.cegeka.tag.tagapi.model.Annotation;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
+@Setter
+@Getter
+@NoArgsConstructor
 public class ImageDTO {
+
   private String id;
   private Map<String, Set<Annotation>> annotations = new HashMap<>();
   private Integer height;
