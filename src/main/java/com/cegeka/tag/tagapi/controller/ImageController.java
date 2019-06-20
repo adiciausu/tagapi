2a3e55883d57cbff05f4a161268d503546c67a9a
Adi Ciausu <adrian.ciausu@cegeka.com>
2019-06-20 19:56:14 +0300

    fixes

--- src/main/java/com/cegeka/tag/tagapi/controller/ImageController.java
+++ src/main/java/com/cegeka/tag/tagapi/controller/ImageController.java
@@ -14,12 +14,14 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -92,6 +94,22 @@ public class ImageController {
   @GetMapping("/image/list")
   @ResponseBody
   public List<ImageDTO> findAll() {
+    List<Image> imageList = this.imageService.findAll();
+    List<ImageDTO> imageDTOList = new ArrayList<>();
+    imageList.forEach((item) -> {
+      ImageDTO imageDTO = modelMapper.map(item, ImageDTO.class);
+      imageDTOList.add(imageDTO);
+    });
+
+    return imageDTOList;
+  }
+
+
+  @RequestMapping(value ="/image/export" )
+  @ResponseBody
+  public List<ImageDTO> txtResponse(HttpServletResponse response){
+    String fileName = "annotations.json";
+    response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
     List<Image> imageList = this.imageService.findAll();
     List<ImageDTO> imageDTOList = new ArrayList<>();
     imageList.forEach((item) -> {
