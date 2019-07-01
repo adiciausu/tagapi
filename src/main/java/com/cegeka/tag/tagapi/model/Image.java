a79a8ae87d3d3b9c14f537c763e1e1298fb2b2e8
Adi Ciausu <adi@Adis-MacBook-Pro-2.local>
2019-07-01 16:31:41 +0300

    fixes

--- src/main/java/com/cegeka/tag/tagapi/model/Image.java
+++ src/main/java/com/cegeka/tag/tagapi/model/Image.java
@@ -1,21 +1,23 @@
 package com.cegeka.tag.tagapi.model;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @Setter
 @Getter
 public class Image {
 
-  @Id
-  private String id;
-  private Map<String, Set<Annotation>> annotations = new HashMap<>();
-  private Integer height;
-  private Integer width;
-  private String name;
-  private String projectId;
+    @Id
+    private String id;
+    private Map<String, Set<Annotation>> annotations = new HashMap<>();
+    private Integer height;
+    private Integer width;
+    private String name;
+    private String projectId;
+    private ImageStatus status;
 }
---
+++ src/main/java/com/cegeka/tag/tagapi/model/ImageStatus.java
@@ -0,0 +1,7 @@
+package com.cegeka.tag.tagapi.model;
+
+public enum ImageStatus {
+    PROCESSING,
+    FINISHED,
+    PENDING
+}
